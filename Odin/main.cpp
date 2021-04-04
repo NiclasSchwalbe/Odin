e812327c09a7ac5733a863c58ca7bc18bf3f21ac
niclas <niclasschwalbe@yahoo.com>
2021-04-04 19:43:20 +0200

    Initial Commit

---
+++ Odin/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Odin
+//
+//  Created by Niclas Schwalbe on 04.04.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
