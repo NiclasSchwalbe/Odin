4c9608e941060baaa146a412e24934f9174c103a
niclas <niclasschwalbe@yahoo.com>
2021-05-15 10:57:57 +0200

    Latest push

--- Odin/util/Utility.cpp
+++ Odin/util/Utility.cpp
@@ -3,20 +3,8 @@
 //
 
 #include "Utility.h"
-#include "Figure.h"
 
-const Figure BPAWN(1, BLACK);
-const Figure BKNIGHT(2, BLACK);
-const Figure BBISHOP(3, BLACK);
-const Figure BROOK(4, BLACK);
-const Figure BQUEEN(5, BLACK);
-const Figure BKING(6, BLACK);
-const Figure WPAWN(1, WHITE);
-const Figure WKNIGHT(2, WHITE);
-const Figure WBISHOP(3, WHITE);
-const Figure WROOK(4, WHITE);
-const Figure WQUEEN(5, WHITE);
-const Figure WKING(6, WHITE);
+
 
 std::vector<std::string> splitString(std::string s, char limiter) {
     std::vector<std::string> tokens;
--- Odin/util/Utility.h
+++ Odin/util/Utility.h
@@ -6,10 +6,25 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <cstdint>
+#include "../engine/Figure.h"
 
 constexpr std::int8_t WHITE = 1;
 constexpr std::int8_t BLACK = 0;
-static const std::string standardBoardFen{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"};
+//static const std::string standardBoardFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+const Figure BPAWN(1, BLACK);
+const Figure BKNIGHT(2, BLACK);
+const Figure BBISHOP(3, BLACK);
+const Figure BROOK(4, BLACK);
+const Figure BQUEEN(5, BLACK);
+const Figure BKING(6, BLACK);
+const Figure WPAWN(1, WHITE);
+const Figure WKNIGHT(2, WHITE);
+const Figure WBISHOP(3, WHITE);
+const Figure WROOK(4, WHITE);
+const Figure WQUEEN(5, WHITE);
+const Figure WKING(6, WHITE);
 
 std::vector<std::string> splitString(std::string s, char limiter);
 
